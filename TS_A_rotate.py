981fce5c76d853bea8991fa7220c911610ec062f
jasondec <jasondec@gmail.com>
2016-08-06 14:42:50 -0700

    included 180 deg rotation for A line.  cleaned up code.

--- TS_A_rotate.py
+++ TS_A_rotate.py
@@ -26,38 +26,45 @@ TS_data['x_working'] = TS_data['X0']
 TS_data['y_working'] = TS_data['Y0']
 TS_data['z_working'] = TS_data['Z0']
 
-plt.plot(TS_data['x_working'],TS_data['y_working'])
-print TS_data['x_working'],TS_data['y_working']
-plt.show()
+## Plot raw points
+plt.scatter(TS_data['x_working'],TS_data['y_working'], color='grey')
+plt.scatter(GPS_data['Lon'],GPS_data['Lat'], color='orange')
+
+
 
 ## Find base points in TS and GPS tables
 # A_base_GPS = GPS_data.loc[GPS_data['Point'] == 'A_base']
 # A_base_TS = TS_data.loc[TS_data['ID'] == 'A_base']
-A_base_GPS = GPS_data.loc[7]
-A_base_TS = TS_data.loc[0]
+A_base_TS_key = 0
+A_base_GPS_key = 7
 
 
 ## Shift all TS points by A_base_GPS - A_base_TS in x,y,z
+A_base_GPS = GPS_data.loc[A_base_GPS_key]
+A_base_TS = TS_data.loc[A_base_TS_key]
+
 delX = A_base_GPS['Lon'] - A_base_TS['X0']
 delY = A_base_GPS['Lat'] - A_base_TS['Y0']
 delZ = A_base_GPS['Elev'] - A_base_TS['Z0']
-# print delX, delY, delZ
 
 TS_data['x_working'] = TS_data['x_working'] + delX
 TS_data['y_working'] = TS_data['y_working'] + delY
 TS_data['z_working'] = TS_data['z_working'] + delZ
 
-# print TS_data
+## Plot shifted points
+plt.scatter(TS_data['x_working'],TS_data['y_working'], color='blue')
+
+
 
 ## Correct TS Line A data by rotating 180 deg
 angle = 180 #degrees
-xBase = TS_data.loc[0]['X0']      # x-coord of baseA
-yBase = TS_data.loc[0]['Y0']      # y-coord of baseA
+xBase = TS_data.loc[A_base_TS_key]['x_working']      # x-coord of baseA
+yBase = TS_data.loc[A_base_TS_key]['y_working']      # y-coord of baseA
 
+TS_data['x_working'],TS_data['y_working'] = rotate_xy(TS_data['x_working'],TS_data['y_working'],xBase,yBase,angle * math.pi/180)
 
-TS_data['X1'],TS_data['Y1'] = rotate_xy(TS_data['X0'],TS_data['Y0'],xBase,yBase,angle * math.pi/180)
-# print TS_data
+## Plot rotated points
+plt.scatter(TS_data['x_working'],TS_data['y_working'], color='red')
+plt.show()
 
-angle = 7
-# print rotate_xy(TS_data['X1'],TS_data['Y0'],xBase,yBase,angle * math.pi/180)
 
